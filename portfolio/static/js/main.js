dcbc21f03c0266d714a7ada56dca762b3206ec05
Jonathan Selander <selander.jonathan@gmail.com>
2014-02-16 15:29:58 +0100

    upgrade

--- portfolio/static/js/main.js
+++ portfolio/static/js/main.js
@@ -3,4 +3,25 @@ $(document).ready(function () {
         type: 'inline',
         midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
     });
+    $('.open-image-popup').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 200 // don't forget to change the duration also in CSS
+		}
+    });
+});
+
+$('#logo-img').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, "fast");
 });
